dc9adb1ade3974f08ca0b6bbb3eb6398ab46bce3
rafeehcp <rafeeh@blend-ed.com>
2022-07-16 15:07:54 +0530

    button added

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,9 +1,12 @@
+import { Button } from 'react-bootstrap';
+
 export default function ExamplePage() {
   return (
     <main>
       <div className="container-fluid">
         <h1>Ready Ready</h1>
         <p>Hello world!</p>
+        <Button>HAHAHA</Button>
       </div>
     </main>
   );
